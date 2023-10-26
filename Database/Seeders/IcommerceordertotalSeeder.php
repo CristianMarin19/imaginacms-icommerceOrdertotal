3e77ba703275dcd8a29de3d16b2242960f9a5d74
William Verde <williamverde10@gmail.com>
2023-10-26 17:37:38 -0400

    Update: seeder repository

--- Database/Seeders/IcommerceordertotalSeeder.php
+++ Database/Seeders/IcommerceordertotalSeeder.php
@@ -47,7 +47,7 @@ class IcommerceordertotalSeeder extends Seeder
                 'en' => ['title' => trans($title,[],'en'),'description' => trans($description,[],'en')]
             );
 
-            $shippingMethodCreated = $shippingMethodRepository->create($params);
+            $shippingMethodCreated =  app('Modules\Icommerce\Repository\ShippingMethodRepository')->create($params);
 
         }
         
